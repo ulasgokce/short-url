af7b1a226ef08d7f939b2324a6343bd865447e2b
Personal <logoscrappertwo@gmail.com>
2023-02-23 16:38:39 +0300

    renameing

--- database/migrations/2023_02_23_224848_update_short_url_visits_table_add_lead_id.php
+++ database/migrations/2023_02_23_224848_update_short_url_visits_table_add_lead_id.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateShortURLTableForVersionTwoZeroZero extends Migration
+class UpdateShortURLVisitsTableAddLeadId extends Migration
 {
     /**
      * Run the migrations.
